1f8adee5919519c0ec781bc1b0392f95c99194b8
Allan Silveira <allansilveira@gmail.com>
2019-08-14 23:42:27 -0300

    teste de upload

---
+++ aula_02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aula_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
