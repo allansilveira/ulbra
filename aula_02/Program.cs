61e64b6c79d5243236bc207fe2e62d2a5a7726a9
Allan Silveira <allansilveira@gmail.com>
2019-08-15 19:32:20 -0300

    conteúdo aula 2

---
+++ aula_02/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace aula_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta objContaDoJoao = new Conta(1234,"João da Silva");
+
+            w($"Num. Conta: {objContaDoJoao.numero}.");
+
+            objContaDoJoao.Depositar(1000);
+            w($"Saldo do: {objContaDoJoao.titular} é {objContaDoJoao.saldo}.");
+             objContaDoJoao.Sacar(500);
+            w($"Saldo do: {objContaDoJoao.titular} é {objContaDoJoao.saldo}.");
+            r();
+
+        }
+
+        static void w(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        static void r()
+        {
+            Console.ReadLine();
+        }
+    }
+}
