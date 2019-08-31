95152be76cad3c83dc843db8caf139dd55325803
Allan Silveira <allansilveira@gmail.com>
2019-08-30 21:34:09 -0300

    aula 06

---
+++ aula06CrudPeople/Controllers/PeopleController.cs
@@ -0,0 +1,47 @@
+using aula06CrudPeople.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aula06CrudPeople.Controllers
+{
+    public class PeopleController : Controller
+    {
+        PersonRepository _repository = 
+            new PersonRepository();
+        public IActionResult Index()
+        {
+            var people = _repository.GetAll();
+            return View(people);
+        }
+        
+        public IActionResult Create()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Create(Person person)
+        {            
+            _repository.Create(person);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {            
+            var pessoa = _repository.GetById(id);
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Edit(Person person)
+        {            
+            _repository.Update(person);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
---
+++ aula_05_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aula_05_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
