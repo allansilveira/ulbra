bc42b6c48c6948de2b53e381a04eabd6190e9427
Allan Silveira <allansilveira@gmail.com>
2019-10-25 19:16:25 -0300

    ToDoList

--- ToDoListDrop/ToDoList.Repository/Migrations/20191025220802_ADDImovelToManyConta.Designer.cs
+++ ToDoListDrop/ToDoList.Repository/Migrations/20191025220802_ADDImovelToManyConta.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ToDoList.Repository;
@@ -9,9 +10,10 @@ using ToDoList.Repository;
 namespace contaluz.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20191025220802_ADDImovelToManyConta")]
+    partial class ADDImovelToManyConta
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ToDoListDrop/ToDoList.Repository/Migrations/20191025220802_ADDImovelToManyConta.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace contaluz.Migrations
+{
+    public partial class ADDImovelToManyConta : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "houseid",
+                table: "Conta",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Imovel",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Imovel", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conta_houseid",
+                table: "Conta",
+                column: "houseid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Conta_Imovel_houseid",
+                table: "Conta",
+                column: "houseid",
+                principalTable: "Imovel",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Conta_Imovel_houseid",
+                table: "Conta");
+
+            migrationBuilder.DropTable(
+                name: "Imovel");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Conta_houseid",
+                table: "Conta");
+
+            migrationBuilder.DropColumn(
+                name: "houseid",
+                table: "Conta");
+        }
+    }
+}
